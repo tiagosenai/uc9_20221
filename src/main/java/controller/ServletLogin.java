09f299c847dff4ca53d487f8e57e37abef2a68fc
Tiago Caceraghi dos Santos <tiago.caceraghi@edu.sc.senai.br>
2023-07-25 21:17:54 -0300

    Versão 0.0.4 - Simulando Login

--- src/main/java/controller/ServletLogin.java
+++ src/main/java/controller/ServletLogin.java
@@ -7,6 +7,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.Usuario;
+
 import java.io.IOException;
 
 @WebServlet("/ServletOi")
@@ -23,14 +25,29 @@ public class ServletLogin extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String usuario = request.getParameter("usuario");
 		String senha = request.getParameter("senha");
+		String url = request.getParameter("url");
 		
 		if (usuario != null && !usuario.isEmpty() && senha != null && !senha.isEmpty()) {
-			//Instaciar o Objeto
-			//Para fazer o login
-			//utilizar a conexão banco
+			Usuario user01 = new Usuario();
+			user01.setUsuario(usuario);
+			user01.setSenha(senha);
+			
+			if (user01.getUsuario().equals("admin") && user01.getSenha().equals("admin")) {
+				request.getSession().setAttribute("usuario", user01);
+				if (url == null || url.equals("null")) {
+					url = "painel/inicio.jsp";
+				}
+				RequestDispatcher redirecionar = request.getRequestDispatcher(url);
+				redirecionar.forward(request, response);
+			}
+			else {
+				//instaciei um objeto
+				RequestDispatcher redireciona = request.getRequestDispatcher("/login.jsp");
+				request.setAttribute("mensagem", "Usuário ou Senha incorretos!");
+				redireciona.forward(request, response);
+			}			
 		}
 		else {
-			
 			//instaciei um objeto
 			RequestDispatcher redireciona = request.getRequestDispatcher("/login.jsp");
 			request.setAttribute("mensagem", "Informe o Usuário e Senha corretamente!");
--- src/main/webapp/login.jsp
+++ src/main/webapp/login.jsp
@@ -9,18 +9,19 @@
 </head>
 <body>
 	<div class="container">
-	<form method="post" action="ServletOi">
-	  <div class="mb-3">
-	    <label for="exampleInputEmail1" class="form-label">Usuário</label>
-	    <input type="email" class="form-control" name="usuario" id="exampleInputEmail1" aria-describedby="emailHelp">
-	  </div>
-	  <div class="mb-3">
-	    <label for="exampleInputPassword1" class="form-label">Senha</label>
-	    <input type="password" name="senha" class="form-control" id="exampleInputPassword1">
-	  </div>
-	  <button type="submit" class="btn btn-primary">Login</button>
-	  <a href="index.jsp" class="btn btn-primary" role="button" aria-disabled="true">Home</a>
-	</form>
+		<form method="post" action="ServletOi">
+	  		<div class="mb-3">
+	    		<label for="exampleInputEmail1" class="form-label">Usuário</label>
+	    		<input type="text" class="form-control" name="usuario" id="exampleInputEmail1" aria-describedby="emailHelp">
+	  		</div>
+	  		<div class="mb-3">
+	    		<label for="exampleInputPassword1" class="form-label">Senha</label>
+	    		<input type="password" name="senha" class="form-control" id="exampleInputPassword1">
+	  		</div>
+	  		<input type="hidden" value="<%= request.getParameter("url") %>" name="url">
+	  		<button type="submit" class="btn btn-primary">Login</button>
+	  		<a href="index.jsp" class="btn btn-primary" role="button" aria-disabled="true">Home</a>
+		</form>
 	<h4> ${mensagem} </h4>
 	</div>
 	
